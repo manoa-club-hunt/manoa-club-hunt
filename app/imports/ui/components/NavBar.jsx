77398fdea780190c31dc2bdbcac80a104fa0d36b
ioaneomerod <ioaneomerod@gmail.com>
2019-11-19 22:30:52 -1000

    Updated navbar and app to link to user profile.

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -17,7 +17,12 @@ class NavBar extends React.Component {
         </Menu.Item>
         <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Clubs</Menu.Item>
         {this.props.currentUser ? (
-            [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Club</Menu.Item>]
+            [
+                <Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Club</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/userprofile" key='userprofile'>
+                Profile
+              </Menu.Item>
+            ]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
