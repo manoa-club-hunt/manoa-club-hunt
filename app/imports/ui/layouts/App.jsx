a4e8aff545e176c0c92e489c4eea17462d754c63
ioaneomerod <51993669+ioaneomerod@users.noreply.github.com>
2019-11-20 13:16:46 -1000

    Fixed PropTypes import.

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import 'semantic-ui-css/semantic.css';
 import { Roles } from 'meteor/alanning:roles';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
