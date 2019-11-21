62a4e4a595648f1f3cf5261db3b95e8ea13f53cc
ioaneomerod <ioaneomerod@gmail.com>
2019-11-21 12:59:29 -1000

    Revert "Made a new user profile page."
    This reverts commit d7077ebe0d8e4e0fb12f79cbf9e1b8c3f378b86f.

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import { Header, Loader, Container, List } from 'semantic-ui-react';
+import { Grid, Segment, Header, Form, Loader } from 'semantic-ui-react';
+import AutoForm from 'uniforms-semantic/AutoForm';
+import TextField from 'uniforms-semantic/TextField';
+import SubmitField from 'uniforms-semantic/SubmitField';
+import swal from 'sweetalert';
 import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
+import SimpleSchema from 'simpl-schema';
 import { Meteor } from 'meteor/meteor';
+import { _ } from 'meteor/underscore';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import MultiSelectField from '../forms/controllers/MultiSelectField';
+import { Interests, interestsName } from '../../api/interests/Interests';
 import { UserProfiles, userProfilesName } from '../../api/userprofiles/UserProfiles';
+import { Clubs, clubsName } from '../../api/club/Club';
+import { updateUserProfileMethod } from '../../startup/both/Methods';
+
+const makeSchema = (allInterests, allClubs) => new SimpleSchema({
+  email: { type: String, label: 'Email', optional: true },
+  firstName: { type: String, label: 'First', optional: true },
+  lastName: { type: String, label: 'Last', optional: true },
+  picture: { type: String, label: 'Picture URL', optional: true },
+  interests: { type: Array, label: 'Interests', optional: true },
+  'interests.$': { type: String, allowedValues: allInterests },
+  clubs: { type: Array, label: 'Clubs', optional: true },
+  'clubs.$': { type: String, allowedValues: allClubs },
+});
 
 class UserProfile extends React.Component {
+
+  submit(data) {
+    Meteor.call(updateUserProfileMethod, data, (error) => {
+      if (error) {
+        swal('Error', error.message, 'error');
+      } else {
+        swal('Success', 'Profile updated', 'success');
+      }
+    });
+  }
+
 
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
@@ -14,23 +46,34 @@ class UserProfile extends React.Component {
 
   renderPage() {
     const email = Meteor.user().username;
+    const allInterests = _.pluck(Interests.find().fetch(), 'interest');
+    const allClubs = _.pluck(Clubs.find().fetch(), 'clubName');
+    const formSchema = makeSchema(allInterests, allClubs);
     const userProfile = UserProfiles.findOne({ email });
+    const model = _.extend({}, userProfile);
     return (
-        <Container>
-          <Header as="h2" textAlign="center">Your Profile</Header>
-          <List>
-            <List.Item><Header as="h3">First Name:</Header></List.Item>
-            <List.Item><p>{userProfile.firstName}</p></List.Item>
-            <List.Item><Header as="h3">Last Name:</Header></List.Item>
-            <List.Item><p>{userProfile.lastName}</p></List.Item>
-            <List.Item><Header as="h3">Interests:</Header></List.Item>
-            <List.Item><p>{userProfile.interests.reduce((memo, interest) => `${memo}, ${interest}`)}</p></List.Item>
-            <List.Item><Header as="h3">Clubs:</Header></List.Item>
-            <List.Item><p>{userProfile.clubs.reduce((memo, interest) => `${memo}, ${interest}`)}</p></List.Item>
-            <List.Item><Header as="h3">Image URL:</Header></List.Item>
-            <List.Item><p>{userProfile.image}</p></List.Item>
-          </List>
-        </Container>
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" textAlign="center">Profile</Header>
+            <AutoForm model={model} schema={formSchema} onSubmit={data => this.submit(data)}>
+              <Segment>
+                <Form.Group widths={'equal'}>
+                  <TextField name='firstName' showInlineError={true} placeholder={'First Name'}/>
+                  <TextField name='lastName' showInlineError={true} placeholder={'Last Name'}/>
+                </Form.Group>
+                <Form.Group widths={'equal'}>
+                  <TextField name='email' showInlineError={true} placeholder={'email'}/>
+                  <TextField name='picture' showInlineError={true} placeholder={'URL to picture'}/>
+                </Form.Group>
+                <Form.Group widths={'equal'}>
+                  <MultiSelectField name='interests' showInlineError={true} placeholder={'Interests'}/>
+                  <MultiSelectField name='clubs' showInlineError={true} placeholder={'Clubs'}/>
+                </Form.Group>
+                <SubmitField value='Save'/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid>
     );
   }
 }
@@ -41,7 +84,9 @@ UserProfile.propTypes = {
 
 export default withTracker(() => {
   const sub1 = Meteor.subscribe(userProfilesName);
+  const sub2 = Meteor.subscribe(clubsName);
+  const sub3 = Meteor.subscribe(interestsName);
   return {
-    ready: sub1.ready(),
+    ready: sub1.ready() && sub2.ready() && sub3.ready(),
   };
 })(UserProfile);
