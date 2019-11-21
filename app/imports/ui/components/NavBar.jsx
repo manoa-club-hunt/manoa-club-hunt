7c17298265e4985d76b438cc905cd4b65a6c638e
ioaneomerod <ioaneomerod@gmail.com>
2019-11-20 15:34:40 -1000

    Revert "Updated navbar with a link to the user home page."
    This reverts commit 263d83c7e726d12981f64a840d9486ac218eee9e.

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -19,10 +19,9 @@ class NavBar extends React.Component {
         {this.props.currentUser ? (
             [
                 <Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Club</Menu.Item>,
-                <Menu.Item as={NavLink} activeClassName="active" exact to="/userhome" key='userhome'>Home</Menu.Item>,
-                <Menu.Item as={NavLink} activeClassName="active" exact to="/userprofile" key='userprofile'>
-                  Profile
-                </Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/userprofile" key='userprofile'>
+                Profile
+              </Menu.Item>,
             ]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
