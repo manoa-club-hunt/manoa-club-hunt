3940f35fe9ca8c15fae94fcd6cc1868a84b570ca
Konapiliahi Canaday <54801556+k-canaday@users.noreply.github.com>
2019-12-02 16:20:10 -1000

    issue #23 : added link to go back to club page

--- app/imports/ui/pages/NotFoundClub.jsx
+++ app/imports/ui/pages/NotFoundClub.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Container, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 /** Render a Not Found page if the user enters a URL that doesn't match any route. */
 class NotFoundClub extends React.Component {
   render() {
     return (
-      <Header as="h2" textAlign="center">
-        <p>Club website not available</p>
-      </Header>
+        <Container textAlign="center">
+          <Header as="h2" textAlign="center">
+            <p>Club website not available</p>
+          </Header>
+          <Link to={'/list'}>Return to Club Listings</Link>
+        </Container>
     );
   }
 }
