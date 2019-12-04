de4044f731fcb08a82ae8b994fa95772a235973c
Konapiliahi Canaday <54801556+k-canaday@users.noreply.github.com>
2019-12-04 02:17:41 -1000

    Removed editclub page from navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -22,9 +22,6 @@ class NavBar extends React.Component {
               <Menu.Item as={NavLink} activeClassName="active" exact to="/userhome" key='userhome'>Home</Menu.Item>,
             ]
         ) : ''}
-        {this.props.currentUser ? (
-            [<Menu.Item as={NavLink} activeClassName="active" exact to="/edit" key='edit'>Edit Club</Menu.Item>]
-        ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
         ) : ''}
