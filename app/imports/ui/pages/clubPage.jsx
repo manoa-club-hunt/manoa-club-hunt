89d14f91008906d3412fbb388145cafa39ab8f12
Sun Young Kim <sunyoungkim1@icloud.com>
2019-12-04 16:54:51 -1000

    each interest leads user to all clubs page where user can search for that interest

--- app/imports/ui/pages/clubPage.jsx
+++ app/imports/ui/pages/clubPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader } from 'semantic-ui-react';
+import { Container, Header, Loader, Button } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Clubs } from '../../api/club/Club';
 
+const _ = require('underscore');
+
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class clubPage extends React.Component {
-
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -17,14 +18,25 @@ class clubPage extends React.Component {
 
   /** UPDATE THIS SECTION TO DISPLAY CLUB DATA */
   renderPage() {
+    // console.log(this.props.clubs.interests);
+    function buttonInterest(data) {
+      return (<Button><Link to ={'/list'}>
+        {data}
+      </Link>
+      </Button>);
+    }
     return (
         <Container>
           <Header as="h1" textAlign="left" >{this.props.clubs.clubName}</Header>
           <Header as="h3" textAlign="left">
-            Interest Area(s): {this.props.clubs.interests.reduce((memo, interest) => ` ${memo} ${interest}`)}</Header>
+            Interest Area(s):
+            {_.map(this.props.clubs.interests, function (val) {
+              return buttonInterest(val);
+            })}
+          </Header>
           <Header as="h3" textAlign="left">Club Info</Header>
           <div className="ui bulleted list">
-            <div className="item">Website: <a href={this.props.clubs.website}>{this.props.clubs.website}</a></div>
+            <div className="item">Website: <a href={this.props.clubs.website}>Click here for site</a></div>
             <div className="item">Contact: {this.props.clubs.contact}</div>
             <div className="item">Email: {this.props.clubs.email}</div>
           </div>
