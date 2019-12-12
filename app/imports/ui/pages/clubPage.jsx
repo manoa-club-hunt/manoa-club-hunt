73b714ed45b219921089216bf115973085fdf295
Sun Young Kim <sunyoungkim1@icloud.com>
2019-12-12 09:30:39 -1000

    edit option only for admin

--- app/imports/ui/pages/clubPage.jsx
+++ app/imports/ui/pages/clubPage.jsx
@@ -39,7 +39,14 @@ class clubPage extends React.Component {
             <div className="item">Contact: {this.props.clubs.contact}</div>
             <div className="item">Email: {this.props.clubs.email}</div>
           </div>
-          <button className="ui button"><Link to={`/edit/${this.props.clubs._id}`}>Edit Club</Link></button>
+          {
+            (Roles.userIsInRole(Meteor.userId(), 'officer') &&
+                _.contains(this.props.clubs.officers, Meteor.user().username)) ||
+            Roles.userIsInRole(Meteor.userId(), 'admin') ?
+                (<button className="ui button"><Link to={`/edit/${this.props.clubs._id}`}>
+                  Edit Club
+                </Link></button>) : ''
+          }
         </Container>
     );
   }
